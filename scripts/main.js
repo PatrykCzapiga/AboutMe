4ddb8424d51b69cbced03afb0ea77809d66ff8b6
PatrykCzapiga <czapi96@gmail.com>
2022-02-18 01:26:27 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function recipes() {
+    let selectedClass = document.querySelector(".recipesWindow");
+    selectedClass.classList.toggle("show");
+}
+function jetBrains() {
+    let selectedClass = document.querySelector(".jetBrainsWindow");
+    selectedClass.classList.toggle("show");
+}
